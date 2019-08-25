cd4cbb61578dcdf0d7242686b4378b1559bccb23
mythofleader <mythofleader@gmail.com>
2019-08-25 22:54:35 +0900

    add util promisify method to fs methods

--- lib/reader.ts
+++ lib/reader.ts
@@ -1,4 +1,9 @@
-import { open as openFile, close as closeFile, read as readFile } from 'fs';
+import fs from 'fs';
+import util from 'util';
+
+const openFile = util.promisify(fs.open);
+const closeFile = util.promisify(fs.close);
+const readFile = util.promisify(fs.read);
 
 interface AGenFileReaderConstructor {
     readonly filePath: string;
@@ -34,69 +39,50 @@ export default class AGenFileReader {
     private readonly readConfig: ReadConfig;
     private readonly EMPTY_CHAR: string = '';
     private chunk?: Chunk;
-    private fd: number | undefined;
+    private fd?: number;
 
     constructor(params: AGenFileReaderConstructor) {
         this.filePath = params.filePath;
         this.delimiter = params.delimiter;
         this.readConfig = {
-            offset: 0, 
+            offset: 0,
             position: 0,
             readCharLen: 1,
         };
     }
 
-    private openFile(): Promise<void> {
-        return new Promise((resolve, reject) => {
-            openFile(this.filePath, 'r', (err: NodeJS.ErrnoException | null, fd: number) => {
-                if (err) return reject(err);
-
-                this.fd = fd;
-                return resolve();
-            });
-        });
+    private async openFile(): Promise<void> {
+        this.fd = await openFile(this.filePath, 'r');
     }
 
-    private closeFile(): Promise<void> {
-        return new Promise((resolve, reject) => {
-            closeFile(this.fd!, (err: NodeJS.ErrnoException | null) => {
-                if (err) return reject(err);
-
-                return resolve();
-            });
-        });
+    private async closeFile(): Promise<void> {
+        if (this.fd) await closeFile(this.fd);
     }
 
-    private readChar(position: number): Promise<string> {
-        return new Promise((resolve, reject) => {
-            const buffer = Buffer.alloc(this.readConfig.readCharLen);
+    private async readChar(position: number): Promise<string> {
+        if (!this.fd) return this.EMPTY_CHAR;
 
-            readFile(this.fd!, buffer, this.readConfig.offset, this.readConfig.readCharLen, position,
-                (err: NodeJS.ErrnoException | null, byteRead: number, results: Buffer) => {
-                    if (err) return reject(err);
+        const buffer = Buffer.alloc(this.readConfig.readCharLen);
+        const result = await readFile(this.fd, buffer, this.readConfig.offset, this.readConfig.readCharLen, position);
 
-                    const char = this.isRead(byteRead)
-                        ? results.toString()
-                        : this.EMPTY_CHAR;
-
-                    return resolve(char);
-            });
-        });
+        return this.isRead(result.bytesRead)
+            ? result.buffer.toString()
+            : this.EMPTY_CHAR;
     }
 
-    private isRead(byteRead: number) {
+    private isRead(byteRead: number): boolean {
         return this.readConfig.readCharLen === byteRead;
     }
 
-    private isEmptyChar(c: string) {
+    private isEmptyChar(c: string): boolean {
         return this.EMPTY_CHAR === c;
     }
 
-    private isDelimiter(c: string) {
+    private isDelimiter(c: string): boolean {
         return this.delimiter === c;
     }
 
-    private nextPosition() {
+    private nextPosition(): void {
         this.readConfig.position++;
     }
 
